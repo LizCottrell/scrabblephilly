062e9b6b292425becc56bf062709695317eaff77
Bill Keller <bill@billkeller.name>
2015-06-23 09:40:24 -0500

    comments out top block instead of bottom block

--- gulpfile.js
+++ gulpfile.js
@@ -5,44 +5,44 @@ var watch = require("gulp-watch");
 var browserSync = require("browser-sync");
 var reload = browserSync.reload;
 
-gulp.task('sass', function(){
-	return gulp.src('styles/scss/main.scss')
-		.pipe(watch('styles/scss/main.scss'))
-		.pipe(sass())
-		.pipe(gulp.dest("styles/."))
-});
-
-gulp.task('serve', function(){
-	browserSync({
-		server: {
-			baseDir: './'
-		}
-	});
-
-	gulp.watch(['*.html', 'styles/main.css', 'js/*.js'], {cwd:'./'}, reload);
-
-});
-
 // gulp.task('sass', function(){
-// 	gulp.src('styles/scss/main.scss')
-// 		.pipe(sass({
-// 			includePaths: ['styles/scss']
-// 		}))
+// 	return gulp.src('styles/scss/main.scss')
+// 		.pipe(watch('styles/scss/main.scss'))
+// 		.pipe(sass())
 // 		.pipe(gulp.dest("styles/."))
 // });
 
 // gulp.task('serve', function(){
-	
-// 	browserSync.init(['styles/main.css','*.html', 'js/*.js'], {
+// 	browserSync({
 // 		server: {
 // 			baseDir: './'
 // 		}
 // 	});
+
+// 	gulp.watch(['*.html', 'styles/main.css', 'js/*.js'], {cwd:'./'}, reload);
+
 // });
 
-// gulp.task('watch', ['sass', 'serve'], function(){
-// 	gulp.watch(['styles/scss/*.scss'], ['sass']);
-// })
+gulp.task('sass', function(){
+	gulp.src('styles/scss/main.scss')
+		.pipe(sass({
+			includePaths: ['styles/scss']
+		}))
+		.pipe(gulp.dest("styles/."))
+});
+
+gulp.task('serve', function(){
+	
+	browserSync.init(['styles/main.css','*.html', 'js/*.js'], {
+		server: {
+			baseDir: './'
+		}
+	});
+});
+
+gulp.task('watch', ['sass', 'serve'], function(){
+	gulp.watch(['styles/scss/*.scss'], ['sass']);
+})
 
 
 // The pipe() function reads data from a readable stream as it becomes available and writes it to a destination writable stream.
