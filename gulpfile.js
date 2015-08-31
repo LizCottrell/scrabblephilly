643f941ee687dd4e285a5d380b53c68e960e26d5
Bill Keller <bill@billkeller.name>
2015-08-31 18:42:29 -0400

    removes commented out tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,6 @@ var watch = require("gulp-watch");
 var browserSync = require("browser-sync");
 var reload = browserSync.reload;
 
-// gulp.task('sass', function(){
-// 	return gulp.src('styles/scss/main.scss')
-// 		.pipe(watch('styles/scss/main.scss'))
-// 		.pipe(sass())
-// 		.pipe(gulp.dest("styles/."))
-// });
-
-// gulp.task('serve', function(){
-// 	browserSync({
-// 		server: {
-// 			baseDir: './'
-// 		}
-// 	});
-
-// 	gulp.watch(['*.html', 'styles/main.css', 'js/*.js'], {cwd:'./'}, reload);
-
-// });
-
 gulp.task('sass', function(){
 	gulp.src('styles/scss/main.scss')
 		.pipe(sass({
