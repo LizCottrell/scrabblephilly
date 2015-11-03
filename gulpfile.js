101bded33cf477480c53970cdaec3270a33b55a9
Bill Keller <bill@billkeller.name>
2015-11-03 17:36:21 -0500

    adds sample of a build task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var watch = require("gulp-watch");
 var browserSync = require("browser-sync");
+var replace = require('gulp-replace');
 var reload = browserSync.reload;
 
 gulp.task('sass', function(){
@@ -14,7 +15,6 @@ gulp.task('sass', function(){
 });
 
 gulp.task('serve', function(){
-	
 	browserSync.init(['stylesheets/main.css','*.html', 'js/*.js'], {
 		server: {
 			baseDir: './'
@@ -22,10 +22,25 @@ gulp.task('serve', function(){
 	});
 });
 
+// Combine multiple tasks into one
 gulp.task('watch', ['sass', 'serve'], function(){
 	gulp.watch(['stylesheets/scss/*.scss'], ['sass']);
-})
+});
 
+
+/* PRODUCTION TASKS
+	- Compile SASS, compressed (single line)
+	- Replace image paths with production ready versions
+*/
+gulp.task('buildcss', function(){
+	gulp.src('stylesheets/scss/main.scss')
+		.pipe(sass({
+			includePaths: ['stylesheets/scss'],
+			outputStyle: 'compressed'
+		}))
+		.pipe(replace('../img/', '/path/on/server/img/'))
+		.pipe(gulp.dest("build/."))
+});
 
 
 
