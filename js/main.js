017284efcbba06dbd4d7c42bc3e774d1a0ba86ac
liz <elizabeth.cottrell@gmail.com>
2016-03-25 14:36:40 -0400

    Styled RSVP modal, started map modal, added js for sticky footer

--- js/main.js
+++ js/main.js
@@ -1,18 +1,42 @@
 $(document).on('ready', function(){   
 
+	
+	// FOOTER BOTTOM-STICK
+	
+	// make .main have a minimum height of viewport minus header + footer 
+	
+	var footerStick = function(){
+		var viewportHeight = $('body').height();
+		var headerHeight = $('header').height();
+		var footerHeight = $('footer').height();
+
+		var mainHeight = viewportHeight - headerHeight - footerHeight;
+
+		$('#main').css('min-height', mainHeight);
+	}
+
+	footerStick();
+	
+	$(window).on('resize', function(){
+		footerStick();
+	});
+
+
+
+
 	// SLIDE-TOGGLE //
 
-  $('.js-menu').on('click', function(){     
-    $(this).parent().find('.js-ul').slideToggle();
+	$('.js-menu').on('click', function(){     
+		$(this).parent().find('.js-ul').slideToggle();
 
-   	if ($(this).hasClass('pickles')){
-	   	if ($(this).text() == 'MORE INFO'){
-	   		$(this).text('LESS INFO');
-	   	} else {
-	   		$(this).text('MORE INFO');
+			if ($(this).hasClass('pickles')){
+		   	if ($(this).text() == 'MORE INFO'){
+		   		$(this).text('LESS INFO');
+		   	} else {
+		   		$(this).text('MORE INFO');
+		   	}
 	   	}
-	   }
-  }); 
+	}); 
 
 
 
@@ -28,14 +52,24 @@ $(document).on('ready', function(){
 
 
 
-	// MODAL //
+	// MODALS //
 
 	$('body').removeClass('modal-on');
 
-	$('.js-modal-open').on('click', function(){
+	
+	var modalOpen = function(whichModalToShow){
+		$('.modal-content').removeClass('modal-visible');
 		$('.modal-wrapper').fadeIn(function(){
 			$('body').addClass('modal-on'); 
+			$('#' + whichModalToShow).addClass('modal-visible');
 		});
+	};
+
+	// requires a data-type attribute on each .js-modal-open
+	$('.js-modal-open').on('click', function(){
+			var whichModalToShow = $(this).data('type');
+			console.log(whichModalToShow);
+			modalOpen(whichModalToShow);
 	});
 
 
@@ -47,7 +81,17 @@ $(document).on('ready', function(){
 
 
 
-	// FAQ SLIDE-TOGGLE //
+	// ANIMATION //
+	
+	$('.js-animate').on('click', function(){
+		$('.b').animate({
+			width: '100%', 
+			opacity: 0.5,
+			'margin-left': 0 
+		}, 0);
+
+		$('.a').hide();
+	});
 
 
 
