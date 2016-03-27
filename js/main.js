3548f0ff66c3952c1580c5a40a84225b22664134
liz <elizabeth.cottrell@gmail.com>
2016-03-27 16:58:20 -0400

    Finished Twitter modal/feed functionality, added Animation.css library

--- js/main.js
+++ js/main.js
@@ -43,23 +43,95 @@ $(document).on('ready', function(){
 	// SUB-MENU ACCORDIAN //
 
 	$(function() {
-		$(".js-ST").accordion({
+		$('.js-ST').accordion({
 			active: false, 
 			collapsible: true,
-			heightStyle: "content"
+			heightStyle: 'content'
 		});
 	});
 
 
 
-	// MODALS //
+	// TWITTER-FEED //
+	
+	//hide all content classes
+	$('.js-twitter-content').hide();
+  $('.js-twitter-close').hide();
+
+  //add the click function
+	$('.js-twitter-open').on('click', function(){
+		$('#b').toggleClass('full-block hover-scale');
+
+		//hide venue content block
+		$('#a').hide();
+
+		//shrink icon, show feed and close-button
+    $('.js-twitter-nav').toggleClass('twitter-animate');
+    $('.js-twitter-content').show();
+    $('.js-twitter-close').show();
+	});
+
+	//close twitter-feed button
+	$('.js-twitter-close').on('click', function(){
+		$('.js-twitter-content').hide();
+		$('.js-twitter-close').hide();
+		$('.js-twitter-nav').toggleClass('twitter-animate');
+		$('#b').toggleClass('full-block hover-scale');
+		$('#a').show();		
+	});
+
+
+
+	// TABS CONTENT //
+
+    //hide all content classes
+  $('.js-tab-content').hide();
+  $('.js-tab-close').hide();
+
+  //add the click function
+  $('.js-tabs-nav li').click(function(){
+    //remove active class from previous li
+    $('.js-tabs-nav li').removeClass('active');
+
+    //add active class to the active li.
+    $(this).addClass('active');
+
+    //hide all content classes
+    $('.js-tab-content').hide();
+
+    //find the data attribute of the active tab
+    var activeTab = $(this).find('button').data('type');
+
+    //fade in the content of active tab
+    $('#' + activeTab).fadeIn(500);
+
+    //shrink tabs to top left corner and display close button
+    $('.js-tabs-nav').addClass('tabs-animate');
+    $('.js-tab-close').show();
+
+    //close tab button
+    $('.js-tab-close').on('click', function(){
+			$('.js-tab-content').fadeOut();
+			$('.js-tabs-nav').removeClass('tabs-animate');
+			$('.js-tabs-nav').fadeIn(500);
+			$('.js-tab-close').hide();
+		});
+
+      return false;
+
+  });
+
+
+
+
+   // MODALS //
 
 	$('body').removeClass('modal-on');
 
 	
 	var modalOpen = function(whichModalToShow){
 		$('.modal-content').removeClass('modal-visible');
-		$('.modal-wrapper').fadeIn(function(){
+		$('.modal-wrapper').show(function(){
 			$('body').addClass('modal-on'); 
 			$('#' + whichModalToShow).addClass('modal-visible');
 		});
@@ -74,68 +146,10 @@ $(document).on('ready', function(){
 
 
 	$('.js-modal-close').on('click', function(){
-		$('.modal-wrapper').fadeOut(function(){
+		$('.modal-wrapper').hide(function(){
 			$('body').removeClass('modal-on'); 
 		});
 	});
-
-
-
-	// TWITTER CONTENT BLOCK ANIMATION //
-	
-	$('.js-animate').on('click', function(){
-		$('.b').animate({
-			width: '100%', 
-			opacity: 0.5,
-			'margin-left': 0 
-		}, 0);
-
-		$('.a').hide();
-	});
-
-
-
-	// TABS
-
-    //hide all content classes.
-    $('.content').hide();
-    $(".js-tab-close").hide();
-
-    //add the click function
-    $('.tabs-nav li').click(function(){
-
-        //remove active class from previous li
-        $('.tabs-nav li').removeClass('active');
-
-        //add active class to the active li.
-        $(this).addClass('active');
-
-        //hide all content classes
-        $(".content").hide();
-
-        //find the data attribute of the active tab
-        var activeTab = $(this).find("button").data("type");
-
-        //fade in the content of active tab
-        $('#' + activeTab).fadeIn(500);
-
-        //shrink tabs to top left corner and display close button
-        $(".js-tabs-nav").addClass('tabs-nav-shrink');
-        $(".js-tab-close").show();
-
-        //close tab button
-        $('.js-tab-close').on('click', function(){
-			$('.content').fadeOut(function(){
-				$('body').removeClass('tab-on'); 
-			});
-			$(".js-tabs-nav").removeClass('tabs-nav-shrink');
-			$(".tabs-nav").fadeIn(500);
-			$(".js-tab-close").hide();
-		});
-
-        return false;
-
-    });
 
 
 
